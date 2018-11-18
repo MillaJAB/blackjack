15c15c12abcf47b0fda8465492608e8e19f2a622
Jenna Johnson <MillaJAB@users.noreply.github.com>
2018-11-17 19:56:43 -0600

    TypeError

--- blackjack.py
+++ blackjack.py
@@ -5,4 +5,28 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 playing = True
 
-print("what the fuck, man")
+class Card:
+	def __init__(self, suit, rank):
+		self.suit = suit
+		self.rank = rank
+	def __str__(self):
+		return "{}".format(self.rank) + " of " + "{}".format(self.suit)
+
+#blah = Card('Spades', 'Ace')
+#print(blah)
+
+class Deck:
+	def __init__(self):
+		self.deck = []
+		for suit in suits:
+			for rank in ranks:
+				pass
+	def __str__(self):
+		print("Some bullshit")
+	def shuffle(self):
+		random.shuffle(self.deck)
+	def deal(self):
+		pass
+
+test_deck = Deck()
+print(test_deck)
